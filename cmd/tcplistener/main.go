9bee83f3c4f307c2a702bd14a35c65d91435d761
Samudra-G <samudramukhar@gmail.com>
2025-10-07 12:13:08 +0000

    changed custom getLineRequest to built in RequestFromReader

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,46 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/Samudra-G/http-golang/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str // new line sent to channel
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			out <- str // sending remaining words
-		}
-	}()
-
-	return out
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -53,9 +19,16 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed with error: %v", err)
 		}
- 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("Failed with error: %v", err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+		 
 	}	
 }
